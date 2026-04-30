93ecf9b56100ff8b495add2efbc76a4294f423a3
Dhanush Doddapaneni <dhanushdoddapaneni21@gmail.com>
2026-04-30 16:49:24 +0530

    cnn+cbam model added

---
+++ src/models/cnn_cbam.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.models.cbam import CBAM
+
+class CNN_CBAM(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.cbam1 = CBAM(32)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.cbam2 = CBAM(64)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.cbam1(x)
+
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.cbam2(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
