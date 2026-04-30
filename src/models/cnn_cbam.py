368df829d371e01f7261a2d11db4bf08cb637acc
Dhanush Doddapaneni <dhanushdoddapaneni21@gmail.com>
2026-04-30 16:52:27 +0530

    modified path error

--- src/models/cnn_cbam.py
+++ src/models/cnn_cbam.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from src.models.cbam import CBAM
+from src.models.cbm import CBAM
 
 class CNN_CBAM(nn.Module):
     def __init__(self):
